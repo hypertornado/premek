3c55884a858746f30e5eb4e6e63abcdebd771743
Ondrej Odchazel <hypertornado@gmail.com>
2014-08-15 13:53:01 +0200

    func

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 
 
 function bindEvents() {
-  console.log("OK");
+  $("textarea")[0].focus();
+
+  $("textarea").on("keydown", handleChange);
+  $("textarea").on("paste", handleChange);
+
 }
 
+function handleChange() {
+  var text = $("textarea").val();
+
+  text = translate(text);
+
+  $("#translated").text(text);
+}
+
+
+function translate(text) {
+  return text + "EE";
+}
 
 $(document).ready(bindEvents)
